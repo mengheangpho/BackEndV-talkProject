022d849514108a86e8f6a1b1d63df9fa5604a3df
mengheangpho <mengheang.pho@student.passerellesnumeriques.org>
2021-05-20 15:23:38 +0700

    first push

---
+++ Public/js/script.js
@@ -0,0 +1,34 @@
+// all variables 
+
+
+// container 
+let startContainer = document.querySelector('.container-start');
+let loginContainer = document.querySelector('.container-login');
+let signinContainer = document.querySelector('.container-signup')
+
+let backbtn = document.querySelector('.backtostart')
+// variable of button 
+const btn_login = document.querySelector('.login-btn');
+const btn_signup = document.querySelector('.singup-btn')
+function login(event){
+    startContainer.style.display="none";
+    loginContainer.style.display="block"
+}
+function signin(event){
+    startContainer.style.display="none";
+    signinContainer.style.display="block"
+}
+
+function backtostart(event){
+    console.log('yes')
+    signinContainer.style.display="none"
+    loginContainer.style.display="none"
+    startContainer.style.display="block"
+}
+
+btn_login.addEventListener('click',login)
+
+
+btn_signup.addEventListener('click',signin)
+
+backbtn.addEventListener('click',backtostart)
