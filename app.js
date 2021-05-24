6ac18ee3fb1e936fd667bcf81bf435466c1e3c2e
mengheangpho <mengheang.pho@student.passerellesnumeriques.org>
2021-05-24 12:22:47 +0700

    change later

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let app=express();
 let port = 3000;
 app.use(express.urlencoded());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("Public"));
 app.listen((process.env.PORT) || port,()=>{
     console.log("Server is running on port "+port)
 })
